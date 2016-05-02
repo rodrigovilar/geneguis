89f16993b68c2316e080dbd9aafa5647c80b7d36
Anderson <andersoonalvess@gmail.com>
2016-05-02 20:55:46 -0300

    Run Selenium WebDriver Test Cases in Chrome.

--- e2e/src/test/java/br/edu/ufcg/embedded/ise/geneguis/WebBrowserTestCase.java
+++ e2e/src/test/java/br/edu/ufcg/embedded/ise/geneguis/WebBrowserTestCase.java
@@ -5,7 +5,8 @@ import java.util.concurrent.TimeUnit;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import br.edu.ufcg.embedded.ise.geneguis.backend.WidgetType;
 
@@ -18,13 +19,26 @@ public abstract class WebBrowserTestCase {
 	static WidgetType Property = WidgetType.Property;
 	
 	
-
-	static FirefoxDriver driver;
+	/**
+	 * To run Selenium WebDriver test cases in Chrome
+	 * you need to download the executable driver from:
+	 * https://sites.google.com/a/chromium.org/chromedriver/downloads
+	 * 
+	 * Use the follow code: 
+	 * 
+	 * System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+	 * WebDriver driver = new ChromeDriver(); 
+	 */
+	
+	static WebDriver driver;
+//	static FirefoxDriver driver;
 
 	@BeforeClass
 	public static void openBrowser() {
 		if (driver == null) {
-			driver = new FirefoxDriver();
+			System.setProperty("webdriver.chrome.driver", "/home/anderson/Downloads/chromedriver");
+			driver = new ChromeDriver();
+//			driver = new FirefoxDriver();
 			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		}
 	}
